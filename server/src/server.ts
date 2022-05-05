0c416e7a032571d4a18cfedb377cc9f2967b6485
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-05-05 13:32:52 -0300

    Install and configure prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,16 @@ import express from 'express'
 
 const app  = express();
 
-app.get('/users', (req, res) => {
+app.use(express.json());
+
+app.post('/feedbacks', (req, res) => {
+    console.log(req.body)
+
     return res.send('Hello World');
+
 })
 
 app.listen(3333, () => {
     console.log('HTTP server running!!!')
 })
+
