1a4be0c0925775def35c04d436940ac83d353939
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-05-08 12:18:50 -0300

    integration with back-end

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, TextInput, Image, Text, TouchableOpacity } from 'react-native';
 import { ArrowLeft } from 'phosphor-react-native';
 import { captureScreen } from 'react-native-view-shot';
+import * as FileSystem from 'expo-file-system';
 
 import { FeedbackType } from  '../../components/Widget'
 import { Button } from  '../../components/Button'
@@ -43,11 +44,12 @@ export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props
         }
 
         setIsSendingFeedback(true);
+        const screenshotBase64 = screenshot && await FileSystem.readAsStringAsync(screenshot, { encoding: 'base64' });
 
         try{ 
            await api.post('/feedbacks', {
                type: feedbackType,
-               screenshot,
+               screenshot: `data: image/png;base64, ${screenshotBase64}`,
                comment
            });
 
