dd747327d8c509ee316068142dae2f3b1948130c
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-05-09 21:36:13 -0300

    fixing deploy

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -39,7 +39,6 @@ export class SubmitFeedbackUseCase {
                 screenshot ? `<img src="${screenshot}" />` : ``,
                 `</div>`
             ].join('\n')
-            
         })
     }
 }
